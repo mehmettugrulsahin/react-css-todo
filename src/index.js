b31563276b2e4e09aaf897084f48e4c7bd50e834
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2017-05-06 22:04:32 +0200

    Moves components into components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {Provider} from 'react-redux';
-import TodoApp from './TodoApp';
+import TodoApp from './components/TodoApp';
 import store from './store';
 
 const router = (
