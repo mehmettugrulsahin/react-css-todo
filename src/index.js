279c44bd7e9c7685176705671a5a62f38ff34798
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2017-05-06 23:00:02 +0200

    01 Prep HTML for Styling with Flat Class Names

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import React from 'react';
 import { render } from 'react-dom';
 import {Provider} from 'react-redux';
@@ -6,7 +7,9 @@ import store from './store/store';
 
 const router = (
     <Provider store={store}>
+    <div className="container">
       <TodoApp />
+    </div>
     </Provider>
 )
 
